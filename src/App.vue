ce5098f5feb05389fd8502795ec7b06dcdbc2a23
yuxuan <m13655692997@163.com>
2017-06-15 13:40:44 +0800

    vue-webpack

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+    <div id="example">
+        <h1>{{ msg }}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            msg: 'Hello World!'
+        }
+    }
+}
+</script>
+
+<style scoped>
+#example {
+    height: 100px;
+    width: 400px;
+    font-size: 24px;
+    background-color: lightblue;
+    text-align: center;
+}
+</style>
